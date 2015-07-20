2de7d72427eb700c0d17f0aeb04a9b2e5e459897
Martin Jambon <martin@mjambon.com>
2015-07-19 23:05:12 -0700

    Put together "one life" simulation.

--- env.ml
+++ env.ml
@@ -18,6 +18,7 @@
      of such lethal accidents is modeled by an exponential distribution.
 
    - reproduction is asexual; parent eventually dies of old age.
+     (uh, we need to find a way to recombine genomes)
 
    - the number of children is proportional to the age of death. The idea
      is that general health results both in more children and longer life.
@@ -33,6 +34,8 @@
    - gene A: influences running speed, critical to survive bear attacks
    - gene B: influences resistance against flu infections
 *)
+
+open Printf
 
 let uniform_distribution () =
   Random.float 1.
@@ -63,3 +66,46 @@ let lambda_b = 1. /. mean_age_of_death_b
 let lambda_b_plus = 1. /. mean_age_of_death_b_plus
 
 let lambda_other = 1. /. mean_age_of_death_other
+
+type genome = {
+  a_plus: bool;
+  b_plus: bool;
+}
+
+let string_of_genome x =
+  sprintf "{%s %s}"
+    (if x.a_plus then "A+" else "A ")
+    (if x.b_plus then "B+" else "B ")
+
+let print_report gen cause age =
+  printf "%s cause %s, age %.2f\n"
+    (string_of_genome gen) cause age
+
+let sim_one gen =
+  (*
+     Obtain age of an individual when first lethal accident
+     for each cause of death (A, B, other) occurs
+  *)
+  let a =
+    exponential_distribution
+      (if gen.a_plus then lambda_a_plus else lambda_a)
+  in
+  let b =
+    exponential_distribution
+      (if gen.b_plus then lambda_b_plus else lambda_b)
+  in
+  let other =
+    exponential_distribution lambda_other
+  in
+  let cause_of_death, age =
+    let sorted =
+      List.sort (fun (_, x) (_, y) -> compare x y)
+        ["A", a;
+         "B", b;
+         "other", other]
+    in
+    match sorted with
+    | (cause, age) :: _ -> cause, age
+    | _ -> assert false
+  in
+  print_report gen cause_of_death age
